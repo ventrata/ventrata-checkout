ed87b29d1786861203cb32379d40607bfa1a1021
Dominik Chrastek <dominik.chrastek@gmail.com>
2020-07-20 12:55:39 +0200

    update dist

--- dist/index.js
+++ dist/index.js
@@ -18,6 +18,14 @@ const VentrataCheckout = zoid_frameworks_js.create({
       type: "function",
       required: true,
     },
+    closeModal: {
+      type: "function",
+      required: true,
+    },
+    viewType: {
+      type: "string",
+      required: true,
+    },
   },
   dimensions: {
     width: "345px",
@@ -25,10 +33,19 @@ const VentrataCheckout = zoid_frameworks_js.create({
   autoResize: { height: true, width: false },
 });
 
+const VENTRATA_ID = "ventrata-checkout";
 class VCheckout extends HTMLElement {
+  constructor() {
+    super();
+
+    this.contentElement = null;
+    this.buttonElement = null;
+    this.type = null;
+  }
+
   connectedCallback() {
     const product = this.getAttribute("product");
-    this.setAttribute("id", "ventrata-checkout");
+    this.setAttribute("id", VENTRATA_ID);
 
     if (product == null) {
       throw new Error('Expected prop "product" to be defined');
@@ -39,15 +56,41 @@ class VCheckout extends HTMLElement {
       throw new Error('Expected prop "token" to be defined');
     }
 
-    VentrataCheckout({
+    const type = this.getAttribute("type") ?? "embedded";
+    this.type = type;
+
+    this.element = VentrataCheckout({
       product: product,
       token: token,
       changeViewType: this.changeViewType,
+      closeModal: this.handleCloseModal,
+      viewType: this.type,
     }).render(this);
+
+    if (this.type === "modal" && this.firstChild) {
+      this.contentElement = document.querySelector(`${VENTRATA_ID} > div`);
+      this.contentElement.style.display = "none";
+      this.firstChild.addEventListener("click", this.handleModalView);
+    }
   }
 
-  changeViewType(viewType) {
-    const el = document.querySelector("#ventrata-checkout iframe");
+  disconnectedCallback() {
+    if (this.type === "modal" && this.firstChild) {
+      this.firstChild.removeEventListener("click", this.handleModalView);
+    }
+  }
+
+  handleModalView = () => {
+    this.contentElement.style.display = "inline-block";
+    this.changeViewType("modal");
+  };
+
+  handleCloseModal = () => {
+    this.contentElement.style.display = "none";
+  };
+
+  changeViewType = (viewType) => {
+    const el = document.querySelector(`${VENTRATA_ID} iframe`);
     if (viewType === "modal") {
       el.style.position = "fixed";
       el.style.zIndex = 999999999;
@@ -55,7 +98,7 @@ class VCheckout extends HTMLElement {
       el.style.position = "static";
       el.style.zIndex = 0;
     }
-  }
+  };
 }
 
 customElements.define("ventrata-checkout", VCheckout);
--- dist/umd.js
+++ dist/umd.js
@@ -4717,6 +4717,14 @@
 	      type: "function",
 	      required: true,
 	    },
+	    closeModal: {
+	      type: "function",
+	      required: true,
+	    },
+	    viewType: {
+	      type: "string",
+	      required: true,
+	    },
 	  },
 	  dimensions: {
 	    width: "345px",
@@ -4724,10 +4732,19 @@
 	  autoResize: { height: true, width: false },
 	});
 
+	const VENTRATA_ID = "ventrata-checkout";
 	class VCheckout extends HTMLElement {
+	  constructor() {
+	    super();
+
+	    this.contentElement = null;
+	    this.buttonElement = null;
+	    this.type = null;
+	  }
+
 	  connectedCallback() {
 	    const product = this.getAttribute("product");
-	    this.setAttribute("id", "ventrata-checkout");
+	    this.setAttribute("id", VENTRATA_ID);
 
 	    if (product == null) {
 	      throw new Error('Expected prop "product" to be defined');
@@ -4738,15 +4755,41 @@
 	      throw new Error('Expected prop "token" to be defined');
 	    }
 
-	    VentrataCheckout({
+	    const type = this.getAttribute("type") ?? "embedded";
+	    this.type = type;
+
+	    this.element = VentrataCheckout({
 	      product: product,
 	      token: token,
 	      changeViewType: this.changeViewType,
+	      closeModal: this.handleCloseModal,
+	      viewType: this.type,
 	    }).render(this);
+
+	    if (this.type === "modal" && this.firstChild) {
+	      this.contentElement = document.querySelector(`${VENTRATA_ID} > div`);
+	      this.contentElement.style.display = "none";
+	      this.firstChild.addEventListener("click", this.handleModalView);
+	    }
 	  }
 
-	  changeViewType(viewType) {
-	    const el = document.querySelector("#ventrata-checkout iframe");
+	  disconnectedCallback() {
+	    if (this.type === "modal" && this.firstChild) {
+	      this.firstChild.removeEventListener("click", this.handleModalView);
+	    }
+	  }
+
+	  handleModalView = () => {
+	    this.contentElement.style.display = "inline-block";
+	    this.changeViewType("modal");
+	  };
+
+	  handleCloseModal = () => {
+	    this.contentElement.style.display = "none";
+	  };
+
+	  changeViewType = (viewType) => {
+	    const el = document.querySelector(`${VENTRATA_ID} iframe`);
 	    if (viewType === "modal") {
 	      el.style.position = "fixed";
 	      el.style.zIndex = 999999999;
@@ -4754,7 +4797,7 @@
 	      el.style.position = "static";
 	      el.style.zIndex = 0;
 	    }
-	  }
+	  };
 	}
 
 	customElements.define("ventrata-checkout", VCheckout);
